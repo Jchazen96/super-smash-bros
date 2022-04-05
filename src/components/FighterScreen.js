f506fb2b6ab0d34d5367ec229fd072534d39e0ae
Justin Chazen <Justinchazen96@gmail.com>
2022-04-05 17:52:00 -0400

    Fighterscreen can change to any character clicked on

--- src/components/FighterScreen.js
+++ src/components/FighterScreen.js
@@ -1,8 +1,8 @@
-const FighterScreen = ({isVisible, setVisible}) => {
+const FighterScreen = ({isVisible, setVisible, selectedCharacter}) => {
     return(
         <div onClick={() => { setVisible(false) }} className="fighter-screen" style={{display: isVisible ? "block" : "none"}}>
-        <h2>Roy</h2>
-        <img width="100" height="100" src="https://www.smashbros.com/assets_v2/img/fighter/roy/main.png" />
+        <h2>{selectedCharacter.name}</h2>
+        <img width="100" height="100" src={`https://www.smashbros.com/assets_v2/img/fighter/${selectedCharacter.name.toLowerCase()}/main.png`}/>
         <p>
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quod, et voluptatibus accusantium praesentium delectus nemo facilis sunt, possimus, illo tempore error ut dignissimos repellendus explicabo molestias placeat voluptatem quae impedit!
         </p>
