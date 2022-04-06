4127f1640fdf1728d6e59c7a21501c7088d2e2b3
Justin Chazen <Justinchazen96@gmail.com>
2022-04-06 13:43:23 -0400

    Added static gradient bg to grid

--- src/App.js
+++ src/App.js
@@ -18,12 +18,15 @@ const characters = [
 function App() {
   const [visible, setVisible] = useState(false)
   const [selectedCharacter, setSelectedCharacter] = useState(characters[0])
+
+
+
   return (
     <div className="App">
       <h1>Fighters</h1>
       <div className="fighters-grid">
         {characters.map((element) => {
-          return(<Fighter name={element.name} color={element.color} setVisible={setVisible} setSelectedCharacter={setSelectedCharacter}/>)
+          return(<Fighter name={element.name} color={element.color} setVisible={setVisible} setSelectedCharacter={setSelectedCharacter} />)
         })}      
       </div>
       <FighterScreen isVisible={visible} setVisible={setVisible} selectedCharacter={selectedCharacter} />
--- src/components/Fighter.js
+++ src/components/Fighter.js
@@ -7,8 +7,8 @@ export default ({ name, color, setVisible, setSelectedCharacter }) => {
         }} 
             className="fighter" 
             style={{
-                backgroundColor: color, 
-                backgroundImage: `url(https://www.smashbros.com/assets_v2/img/fighter/${name.toLowerCase()}/main.png)`}}>
+                backgroundImage: `url(https://www.smashbros.com/assets_v2/img/fighter/${name.toLowerCase()}/main.png), linear-gradient(140deg, ${color}, lightyellow, lightskyblue)`
+                }}>
             <h4>{name}</h4>
         </div>
     )
